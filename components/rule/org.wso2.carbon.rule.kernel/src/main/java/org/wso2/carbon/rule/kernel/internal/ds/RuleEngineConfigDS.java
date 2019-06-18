f3c23b0c64ff5b93c573a0c228e2540224e91cc4
senthalan <senthalank@gmail.com>
2019-06-18 11:06:58 +0530

    Change the scr doc comment to component annotation

--- components/rule/org.wso2.carbon.rule.kernel/src/main/java/org/wso2/carbon/rule/kernel/internal/ds/RuleEngineConfigDS.java
+++ components/rule/org.wso2.carbon.rule.kernel/src/main/java/org/wso2/carbon/rule/kernel/internal/ds/RuleEngineConfigDS.java
@@ -13,51 +13,56 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.wso2.carbon.rule.kernel.internal.ds;
 
-import org.osgi.service.component.ComponentContext;
-import org.wso2.carbon.rule.kernel.internal.build.RuleEngineConfigBuilder;
-import org.wso2.carbon.rule.kernel.internal.config.CarbonRuleEngineConfigService;
-import org.wso2.carbon.rule.kernel.config.RuleEngineConfigService;
-import org.wso2.carbon.rule.common.exception.RuleConfigurationException;
-import org.wso2.carbon.event.core.EventBroker;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.wso2.carbon.event.core.EventBroker;
+import org.wso2.carbon.rule.common.exception.RuleConfigurationException;
+import org.wso2.carbon.rule.kernel.config.RuleEngineConfigService;
+import org.wso2.carbon.rule.kernel.internal.build.RuleEngineConfigBuilder;
+import org.wso2.carbon.rule.kernel.internal.config.CarbonRuleEngineConfigService;
 
-/**
- * @scr.component name="ruleengineconfig.component" immediate="true"
- * @scr.reference name="eventbroker.service"
- * interface="org.wso2.carbon.event.core.EventBroker" cardinality="1..1"
- * policy="dynamic" bind="setEventBroker" unbind="unSetEventBroker"
- */
+@Component(
+        name = "ruleengineconfig.component",
+        immediate = true)
 public class RuleEngineConfigDS {
 
     private static Log log = LogFactory.getLog(RuleEngineConfigDS.class);
 
+    @Activate
     protected void activate(ComponentContext context) {
 
         RuleEngineConfigBuilder ruleEngineConfigBuilder = new RuleEngineConfigBuilder();
-
         try {
-
-            RuleEngineConfigService ruleEngineConfigService = RuleValueHolder.getInstance().getRuleEngineConfigService();
+            RuleEngineConfigService ruleEngineConfigService = RuleValueHolder.getInstance()
+                    .getRuleEngineConfigService();
             if (ruleEngineConfigService == null) {
-                ruleEngineConfigService =
-                        new CarbonRuleEngineConfigService(ruleEngineConfigBuilder.getRuleConfig());
+                ruleEngineConfigService = new CarbonRuleEngineConfigService(ruleEngineConfigBuilder.getRuleConfig());
                 RuleValueHolder.getInstance().registerRuleEngineConfigService(ruleEngineConfigService);
-
             }
             context.getBundleContext().registerService(RuleEngineConfigService.class.getName(),
-                            ruleEngineConfigService, null);
+                    ruleEngineConfigService, null);
             log.info("Successfully registered the Rule Config service");
         } catch (RuleConfigurationException e) {
             log.error("Can not create the Rule Config service ", e);
         }
-
     }
 
+    @Reference(
+            name = "eventbroker.service",
+            service = org.wso2.carbon.event.core.EventBroker.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unSetEventBroker")
     protected void setEventBroker(EventBroker eventBroker) {
+
         try {
             RuleValueHolder.getInstance().registerEventBroker(eventBroker);
         } catch (RuleConfigurationException e) {
@@ -68,5 +73,4 @@ public class RuleEngineConfigDS {
     protected void unSetEventBroker(EventBroker eventBroker) {
 
     }
-
 }
